1cdd7e55902f6674a26d3ea071a6f7a29515920b
SamyIOoOI <samytonygo@gmail.com>
2025-09-15 18:15:28 +0300

    basic gui elements

--- Pathway.py
+++ Pathway.py
@@ -7,13 +7,23 @@ import json
 import os
 ## Functions
 def sched_button():
-    print("Hello world")
+    print("Hello World!")
+def chores_button():
+    print("Hello World!")
+def community_button():
+    print("Hello World!")
 ## Below is the basic GUI
 gui = TkinterDnD.Tk()
 gui.title("Pathway")
 gui.geometry("500x300")
-tk.messagebox.showinfo("Pathway", "Welcome to pathway! Your personal project organizer!")
 scheduler = tk.Button(text='Scheduler', command=sched_button)
+chores = tk.Button(text="Chores", command=chores_button)
+community = tk.Button(text="Community Service", command=community_button)
 scheduler.config(height=1, width=6)
-scheduler.place(x='40',y='180')
+chores.config(height=1, width=6)
+community.config(height=1, width=14)
+scheduler.place(x='60',y='180')
+community.place(x='180', y='180')
+chores.place(x='360', y='180')
+tk.messagebox.showinfo("Pathway", "Welcome to pathway! Your personal project organizer!")
 gui.mainloop()
